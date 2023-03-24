143e090706a7b4b321c55bb58e7826b3b5d9e619
izilla <isaiahgriego@gmail.com>
2023-03-23 18:12:59 -0600

    Router

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,14 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 import { SignInButton, SignOutButton, useUser } from "@clerk/nextjs";
+import { api } from "~/utils/api";
 
 const Home: NextPage = () => {
   const user = useUser()
+  const { data } = api.posts.getAll.useQuery()
+
+  console.log(data)
+
   return (
     <>
       <Head>
@@ -12,8 +17,15 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c]">
-        {!!user.isSignedIn && <SignOutButton />}
-        {!user.isSignedIn && <SignInButton />}
+        <div>
+          {!!user.isSignedIn && <SignOutButton />}
+          {!user.isSignedIn && <SignInButton />}
+        </div>
+        <div>
+          {data?.map(post => (
+            <div key={post.id}>{post.content}</div>
+          ))}
+        </div>
       </main>
     </>
   );
