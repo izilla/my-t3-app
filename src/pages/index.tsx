7b18e2ead58e0651526dc525d7cccf1e4a3018d6
izilla <isaiahgriego@gmail.com>
2023-03-24 17:22:48 -0600

    Links for profile  and posts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,7 @@ import Image from "next/image";
 import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
 import { toast } from "react-hot-toast";
+import Link from "next/link";
 
 dayjs.extend(relativeTime)
 
@@ -89,11 +90,15 @@ const PostView = (props: PostWithUser) => {
       />
       <div className="flex flex-col">
         <div className="flex text-slate-300 gap-1">
-          <span className="font-semibold">
-            {`@${author.username}`}
-          </span>
+          <Link href={`/@${author.username}`}>
+            <span className="font-semibold">
+              {`@${author.username}`}
+            </span>
+          </Link>
           ·
-          <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
+          <Link href={`/post/${post.id}`}>
+            <span className="font-thin">{dayjs(post.createdAt).fromNow()}</span>
+          </Link>
         </div>
         <span className="text-2xl">
           {post.content}
